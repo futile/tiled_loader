b904d096ca4988770713f901cc6b18a7a304d446
Felix Rath <felixr@archlinux.info>
2016-05-20 16:37:01 +0200

    deserialize properties as HashMaps
    they represent maps, so load them as maps.

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,14 @@
 #![plugin(serde_macros)]
 
 extern crate serde_xml;
+extern crate serde;
 
+use std::collections::HashMap;
 use std::path::Path;
 use std::fs::File;
 use std::io::Read;
+
+use serde::de;
 
 pub type XmlError = serde_xml::Error;
 
@@ -17,24 +21,78 @@ pub struct Image {
     pub height: u32,
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Property {
-    pub name: String,
-    #[serde(rename(deserialize="type"))]
-    pub type_: Option<String>,
-    pub value: String,
+#[derive(Debug)]
+pub enum Property {
+    String(String),
+    Bool(bool),
+    Float(f64),
+    Int(i64),
 }
 
-#[derive(Debug, Deserialize)]
-pub struct Properties {
-    #[serde(rename(deserialize="property"))]
-    properties: Vec<Property>,
+pub type Properties = HashMap<String, Property>;
+
+fn deserialize_properties<D: de::Deserializer>(deserializer: &mut D)
+                                               -> Result<Option<Properties>, D::Error> {
+    #[derive(Debug, Deserialize)]
+    pub struct RawProperty {
+        pub name: String,
+        #[serde(rename(deserialize="type"))]
+        #[serde(default)]
+        pub type_: String,
+        pub value: String,
+    }
+
+    #[derive(Debug, Deserialize)]
+    pub struct RawProperties {
+        #[serde(rename(deserialize="property"))]
+        properties: Vec<RawProperty>,
+    }
+
+    let raw_props: RawProperties = try!(de::Deserialize::deserialize(deserializer));
+    let mut props = Properties::new();
+
+    use std::num::ParseFloatError;
+    use std::num::ParseIntError;
+    use std::str::ParseBoolError;
+    use std::error::Error;
+
+    for raw_prop in raw_props.properties {
+        if props.contains_key(&raw_prop.name) {
+            return Err(de::Error::custom(format!("property '{}' was found twice", raw_prop.name)));
+        }
+
+        let val = match &raw_prop.type_[..] {
+            "" | "string" => Property::String(raw_prop.value),
+            "float" => {
+                Property::Float(try!(raw_prop.value.parse().map_err(|e: ParseFloatError| {
+                    de::Error::custom(e.description())
+                })))
+            }
+            "bool" => {
+                Property::Bool(try!(raw_prop.value.parse().map_err(|e: ParseBoolError| {
+                    de::Error::custom(e.description())
+                })))
+            }
+            "int" => {
+                Property::Int(try!(raw_prop.value.parse().map_err(|e: ParseIntError| {
+                    de::Error::custom(e.description())
+                })))
+            }
+            s => return Err(de::Error::custom(format!("unexpected property type: '{}'", s))),
+        };
+
+        props.insert(raw_prop.name, val);
+    }
+
+    Ok(Some(props))
 }
 
 #[derive(Debug, Deserialize)]
 pub struct Tile {
     id: u32,
 
+    #[serde(deserialize_with="deserialize_properties")]
+    #[serde(default)]
     properties: Option<Properties>,
     image: Option<Image>,
 }
@@ -58,7 +116,7 @@ pub enum Orientation {
     Orthogonal,
     Isometric,
     Hexagonal,
-    Shifted
+    Shifted,
 }
 
 #[derive(Debug, Deserialize)]
@@ -73,10 +131,8 @@ pub struct Layer {
 #[derive(Debug, Deserialize)]
 pub struct Data {
     pub encoding: Option<String>,
-    pub compression: Option<String>,
-
-    #[serde(rename(deserialize="$value"))]
-    pub value: Option<String>,
+    pub compression: Option<String>, /* #[serde(rename(deserialize="$value"))]
+                                      * pub value: Option<String>, */
 }
 
 #[derive(Debug, Deserialize)]
@@ -101,22 +157,23 @@ pub struct Map {
     #[serde(rename(deserialize="layer"))]
     pub layers: Vec<Layer>,
 
+    #[serde(deserialize_with="deserialize_properties")]
+    #[serde(default)]
     pub properties: Option<Properties>,
 }
 
 impl Map {
     pub fn load<P: AsRef<Path>>(path: P) -> Result<Map, XmlError> {
-        let mut file = File::open(path)?;
+        let mut file = File::open(path)?;;
         let mut content = String::new();
-        file.read_to_string(&mut content)?;
+        file.read_to_string(&mut content)?;;
 
-        Ok(serde_xml::from_str(&content)?)
+        Ok(serde_xml::from_str(&content)?))
     }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-    }
+    fn it_works() {}
 }
