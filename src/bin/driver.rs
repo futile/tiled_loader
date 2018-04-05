53e3a6e142956111bd3544c161ba3b62667d7302
Felix Rath <felix.rath@comsys.rwth-aachen.de>
2018-04-05 20:38:05 +0200

    make driver able to deserialize individual maps

--- src/bin/driver.rs
+++ src/bin/driver.rs
@@ -4,13 +4,23 @@ extern crate walkdir;
 use walkdir::{DirEntry, WalkDir};
 
 fn main() {
-    for entry in WalkDir::new("assets") {
-        let entry: DirEntry = entry.unwrap();
-        if entry.file_type().is_file() && entry.file_name().to_str().unwrap().ends_with(".tmx") {
-            match tiled_loader::load_from_path(entry.path()) {
-                Ok(_) => println!("{}: success", entry.path().display()),
-                Err(ref e) => println!("{}: {}", entry.path().display(), e),
-            };
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() == 2 {
+        println!("{:#?}", tiled_loader::load_from_path(&args[1]));
+    } else if args.len() == 1 {
+        for entry in WalkDir::new("assets") {
+            let entry: DirEntry = entry.unwrap();
+            if entry.file_type().is_file() && entry.file_name().to_str().unwrap().ends_with(".tmx")
+            {
+                match tiled_loader::load_from_path(entry.path()) {
+                    Ok(_) => println!("{}: success", entry.path().display()),
+                    Err(ref e) => println!("{}: {}", entry.path().display(), e),
+                };
+            }
         }
+    } else {
+        println!("usage: ./{} [path-to-tmx]", &args[0]);
+        std::process::exit(1);
     }
 }
